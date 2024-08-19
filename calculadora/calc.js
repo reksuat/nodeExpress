ba27ca6045dc9defa8666a49048c2a2ded2418ec
Priscila <pyea08@gmail.com>
2024-08-19 14:19:35 -0300

    calculadora rotas

--- calculadora/calc.js
+++ calculadora/calc.js
@@ -1,6 +1,15 @@
-function soma(n1,n2){ 
-    n1+n2;
+const soma = (n1,n2) => n1+n2
+const subtracao = (n1,n2) => n1-n2
+const multiplicacao = (n1,n2) => n1*n2
+const divisao = (n1,n2) => {
+    if (n2===0) {
+        return "A divisão não pode ser 0"
+    }
+    return n1/n2;
 }
-function subtracao(params) {
-    n1-n2
+module.exports = {
+    soma,
+    subtracao,
+    multiplicacao,
+    divisao
 }
--- calculadora/index.js
+++ calculadora/index.js
@@ -1,11 +1,41 @@
 const express = require("express");
+const { soma, subtracao, multiplicacao, divisao } = require("./calc");
+
 const app = express();
 const porta = 5000;
-app.get('/calculadora/:n1/:n2', (req,res) =>{
-    const n1 = parseFloat(req.params.n1);
-    const n2 = parseFloat(req.params.n2);
-})
+app.use(express.json())
 
+app.get('/calculadora/:operacao/:n1/:n2', (req,res) =>{
+    const { operacao, n1, n2 } = req.params;
+    const num1 = parseFloat(n1);
+    const num2 = parseFloat(n2);
+
+let resultado
+switch (operacao) {
+    case "+":
+        resultado= soma(num1,num2)
+        break;
+        case "-":
+            resultado= subtracao(num1,num2)
+            break;
+            case "x":
+                resultado= multiplicacao(num1,num2)
+                break;
+                case "/":
+                resultado= divisao(num1,num2)
+    default:
+        return res.status(400).json({ erro: 'Operação inválida' });
+}
+res.json({ resultado });
+});
+app.get('/calculadora', (req, res) => {
+    res.json({
+        mensagem: 'Bem-vindo à API da calculadora. Utilize a rota /calculadora/:operacao/:n1/:n2 para realizar operações matemáticas.',
+        operacao: ['+', '-', 'x', '/']
+    });
+});
 app.listen(porta, ()=> {
     console.log(`O servidor iniciou a porta ${porta}`);
-})
+}) 
+
+
